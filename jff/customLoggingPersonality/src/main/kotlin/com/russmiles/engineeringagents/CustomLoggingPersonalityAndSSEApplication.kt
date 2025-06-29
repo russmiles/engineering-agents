76a3d0287a8d84f78fff161b743ff2e723dba630
Russ Miles <russ@russmiles.com>
2025-06-29 11:55:22 +0100

    Added custom Lebowski logging personality

---
+++ jff/customLoggingPersonality/src/main/kotlin/com/russmiles/engineeringagents/CustomLoggingPersonalityAndSSEApplication.kt
@@ -0,0 +1,15 @@
+package com.russmiles.engineeringagents
+
+import com.embabel.agent.config.annotation.EnableAgentShell
+import com.embabel.agent.config.annotation.EnableAgents
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableAgentShell
+@EnableAgents(loggingTheme = "lebowski")
+class CustomLoggingPersonalityAndSSEApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomLoggingPersonalityAndSSEApplication>(*args)
+}
---
+++ jff/customLoggingPersonality/src/main/kotlin/com/russmiles/engineeringagents/web/MagazineController.kt
@@ -0,0 +1,53 @@
+package com.russmiles.engineeringagents.web
+
+import com.embabel.agent.api.common.autonomy.Autonomy
+import com.embabel.agent.core.ProcessOptions
+import com.embabel.agent.core.Verbosity
+import com.embabel.agent.event.logging.LoggingPersonality
+import com.embabel.agent.shell.formatProcessOutput
+import com.embabel.common.util.color
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.slf4j.Logger
+import org.springframework.web.bind.annotation.PathVariable
+
+@RestController
+class MagazineController (
+    private val loggingPersonality: LoggingPersonality,
+    private val autonomy: Autonomy,
+    private val objectMapper: ObjectMapper
+) {
+
+    private val logger: Logger = loggingPersonality.logger
+
+    @GetMapping("/")
+    fun welcome(): String {
+        return "Welcome."
+    }
+
+    @GetMapping("/tellmeastory/{topic}", produces = ["application/json"])
+    fun tellMeAStoryOnTopic(@PathVariable topic:String): String {
+        val intent = "Tell me a story about ${topic}"
+        logger.info(intent)
+        val verbosity = Verbosity(
+            debug = false,
+            showPrompts = true,
+            showLlmResponses = false,
+            showPlanning = true,
+        )
+        val output = autonomy.chooseAndRunAgent(
+            intent = intent,
+            processOptions = ProcessOptions(
+                test = false,
+                verbosity = verbosity,
+            ))
+        logger.info("executed \"$intent\"".color(loggingPersonality.colorPalette.color2))
+        logger.info("using object mapper \"$objectMapper\"")
+        return formatProcessOutput(
+            output,
+            loggingPersonality.colorPalette,
+            objectMapper,
+            80)
+    }
+}
