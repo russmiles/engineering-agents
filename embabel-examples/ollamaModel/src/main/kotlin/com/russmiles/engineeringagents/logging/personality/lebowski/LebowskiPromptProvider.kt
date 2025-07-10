83b3865aee5248d2ec388ce523b077377c52ee68
Russ Miles <russ@russmiles.com>
2025-07-10 13:27:57 +0100

    Added a simple by-name ollama local model selection example
    Closes #7

---
+++ embabel-examples/ollamaModel/src/main/kotlin/com/russmiles/engineeringagents/logging/personality/lebowski/LebowskiPromptProvider.kt
@@ -0,0 +1,33 @@
+package com.russmiles.engineeringagents.logging.personality.lebowski
+
+import com.embabel.agent.shell.MessageGeneratorPromptProvider
+import com.embabel.common.util.RandomFromFileMessageGenerator
+import com.embabel.common.util.bold
+import com.embabel.common.util.color
+import com.embabel.common.util.italic
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+fun guide(text: String) = "${" Scene ".bold()} ${text.italic()}"
+
+val SceneLocations = listOf(
+    "The Dude’s Apartment",
+    "Hollywood Star Lanes bowling alley",
+    "Lebowski’s Mansion",
+    "Sobchak Security",
+    "Wooden bridge",
+    "Maude’s Loft",
+    "Crane Jackson’s Fountain Street Theater",
+    "Jackie Treehorn’s Beach Party",
+    "Jackie Treehorn’s Mansion",
+    "Donny’s ashes scattering spot",)
+
+@Component
+@Profile("lebowski")
+class LebowskiPromptProvider : MessageGeneratorPromptProvider(
+    color = LebowskiColorPalette.KNITWEAR,
+    prompt = SceneLocations.random(),
+    messageGenerator = RandomFromFileMessageGenerator(
+        url = "logging/lebowski.txt"
+    ),
+)
